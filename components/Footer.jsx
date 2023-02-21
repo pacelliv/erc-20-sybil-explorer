39e89111f8e5ca2bce62d56b547e2f351122c885
PacelliV <flores.eugenio03@gmail.com>
2023-02-21 18:55:12 -0500

    replaced png for react-icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,7 @@
 import Link from "next/link"
 import styled from "styled-components"
+import { FaGithub, FaTwitter } from "react-icons/fa"
+import { BsArrowUpRightSquareFill } from "react-icons/bs"
 
 const Div = styled.div`
     position: absolute;
@@ -27,24 +29,26 @@ const Div = styled.div`
     filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#0061FF", endColorstr="#60EFFF", GradientType=1 );
 
     width: 100%;
-    padding: 0.75em 2.2em 0.4em 1.5em;
+    padding: 0.8em 2em 0.5em;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     gap: 10px;
 
     .logo {
-        width: 20px;
+        font-size: 20px;
+    }
+
+    .margin {
+        margin-left: 0.5em;
     }
 
     .link {
         display: flex;
-        align-items: center;
-        border-bottom: 1px dashed black;
         font-size: 0.8rem;
-        margin: auto 0 0.6em auto;
         gap: 5px;
     }
     .link a {
+        margin-top: 0.1em;
         font-weight: 500;
     }
 
@@ -53,19 +57,22 @@ const Div = styled.div`
     }
 
     .link .logo {
-        width: 15px;
+        font-size: 17px;
     }
 `
 
 const Footer = () => {
     return (
         <Div>
-            <Link href="https://github.com/pacelliv" target="_blank">
-                <img className="logo" src="../github.png" />
-            </Link>
-            <Link href="https://twitter.com/pacelliv3" target="_blank">
-                <img className="logo" src="../twitter.png" />
-            </Link>
+            <div>
+                <Link href="https://github.com/pacelliv" target="_blank">
+                    <FaGithub className="logo" />
+                </Link>
+                <Link href="https://twitter.com/pacelliv3" target="_blank">
+                    <FaTwitter className="logo margin" />
+                </Link>
+            </div>
+
             <div className="link">
                 <a
                     href="https://github.com/pacelliv/erc20-sybil-token"
@@ -73,10 +80,7 @@ const Footer = () => {
                 >
                     Fork on Github
                 </a>
-                <img
-                    className="logo"
-                    src="../square-arrow-up-right-solid.svg"
-                />
+                <BsArrowUpRightSquareFill className="logo" />
             </div>
         </Div>
     )
