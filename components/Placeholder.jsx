6d188a9f62a39f0c5643ecae66a19063fa8b04ab
PacelliV <flores.eugenio03@gmail.com>
2023-02-24 22:54:46 -0500

    replaced png for icons

--- components/Placeholder.jsx
+++ components/Placeholder.jsx
@@ -1,29 +1,42 @@
 import styled from "styled-components"
+import { FiWifiOff } from "react-icons/fi"
+import { MdSignalWifiConnectedNoInternet4 } from "react-icons/md"
+import { TbPlugConnectedX, TbWalletOff } from "react-icons/tb"
+import { ImBlocked } from "react-icons/im"
 
 const Div = styled.div`
-    margin: 5em auto 0;
+    margin: 8em auto 0;
     max-width: 650px;
     border: medium dashed #e8e8e8;
     border-radius: 30px;
-    padding: 2.5em 2em 2.5em 1em;
+    padding: 3em 2em 3em 1em;
     display: flex;
+    flex-direction: column;
     align-items: center;
-    gap: 10px;
+    gap: 20px;
     line-height: 1.4;
+
+    .icons-container {
+        display: flex;
+        gap: 8px;
+    }
+
+    .icons {
+        font-size: 15px;
+    }
 
     p {
         font-weight: 500;
         font-size: 0.9rem;
     }
 
-    img {
-        display: block;
-        width: 50%;
-    }
-
     @media (min-width: 530px) {
         p {
             font-size: 1.2rem;
+        }
+
+        .icons {
+            font-size: 22px;
         }
     }
 `
@@ -31,8 +44,16 @@ const Div = styled.div`
 const Placeholder = () => {
     return (
         <Div>
-            <img src="../no-connection.png" />
-            <p>Connect wallet to Ethereum Göerli to launch the app.</p>
+            <div className="icons-container">
+                <FiWifiOff className="icons" />
+                <MdSignalWifiConnectedNoInternet4 className="icons" />
+                <TbPlugConnectedX className="icons" />
+                <TbWalletOff className="icons" />
+                <ImBlocked className="icons" />
+            </div>
+            <p>
+                <span>Connect wallet</span> to Ethereum Göerli to launch the app
+            </p>
         </Div>
     )
 }
